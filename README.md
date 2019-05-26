6f4c15544c6faa6c117f318cb7f881441c84ce90
C.Y. Fang <s47548081@gmail.com>
2019-05-26 13:26:30 +0800

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# TurboHUD
+config
---
+++ interfaces/data/player/IPlayerDamageInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace Turbo.Plugins
+{
+    public interface IPlayerDamageInfo
+    {
+        long TotalDamage { get; set; }
+        long RunDps { get; set; }
+
+        long CurrentDps { get; set; }
+        long MaximumDps { get; set; }
+    }
+}
